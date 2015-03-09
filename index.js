930ffae0399500c0e067962f8eb6732cda2dfbbb
ri <git@tnu.jp>
2015-03-09 19:14:05 +0900

    insert initial conf on js

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 (function(){
-  function parseConfiguration(){
+  function insertSeedLineConfiguration(){
+    var seedLines = [{interval:10,lineDash:2,writeText:false},
+                {interval:100,lineDash:1,writeText:true}];
+    var rows = seedLines.map(function(line){
+      var row = $("<tr>",{class:"row-line-configure"});
+       row.append(
+           $("<td>").append($("<input>",
+               {type:"text",class:"val-interval",value:line.interval}))
+           );
+       row.append(
+           $("<td>").append($("<input>",
+               {type:"text",class:"val-lineDash",value:line.lineDash}))
+           );
+       row.append(
+           $("<td>").append($("<input>",
+               {type:"checkbox",class:"val-writeText",checked:line.writeText}))
+           );
+       return row;
+    });
+
+    $("#table-line-configures").append(rows);
+  }
+
+  function parseCanvasConfiguration(){
+
+  }
+
+  function parseLineConfiguration(){
     return $(".row-line-configure").map(function(_,elem){
-      var interval = $(elem).find(".val-interval").first().prop("value");
-      var lineDash = $(elem).find(".val-lineDash").first().prop("value");
-      var writeText = $(elem).find(".val-writeText").first().prop("checked");
+      var interval = $(elem).find(".val-interval").prop("value");
+      var lineDash = $(elem).find(".val-lineDash").prop("value");
+      var writeText = $(elem).find(".val-writeText").prop("checked");
       return {interval:parseInt(interval),
         lineDash:parseInt(lineDash),
         writeText:writeText};
@@ -11,16 +38,16 @@
   }
 
   function generate(){
-    var conf = parseConfiguration();
+    var lineConf = parseLineConfiguration();
     var canvasContainer = $("#canvas-container");
-    var pg = new Phogen(conf);
+    var pg = new Phogen(lineConf);
 
     var renderedCanvas = pg.render(400,300);
 
     canvasContainer.empty();
     canvasContainer.append(renderedCanvas);
   }
-
+  insertSeedLineConfiguration();
   $("#generate-btn").click(function(){
     generate();
   });
