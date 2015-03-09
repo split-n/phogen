d99f16186ab413101c0aca99c7b2788db1c717e0
ri <git@tnu.jp>
2015-03-09 18:13:18 +0900

    configurable

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 (function(){
+  function parseConfiguration(){
+    return $(".row-line-configure").map(function(_,elem){
+      var interval = $(elem).find(".val-interval").first().prop("value");
+      var lineDash = $(elem).find(".val-lineDash").first().prop("value");
+      var writeText = $(elem).find(".val-writeText").first().prop("checked");
+      return {interval:parseInt(interval),
+        lineDash:parseInt(lineDash),
+        writeText:writeText};
+    }).toArray();
+  }
+
   function generate(){
+    var conf = parseConfiguration();
     var canvasContainer = $("#canvas-container");
-    var pg = new Phogen([
-        {interval:10,lineDash:2,writeText:false},
-        {interval:100,lineDash:1,writeText:true} ]);
+    var pg = new Phogen(conf);
 
     var renderedCanvas = pg.render(400,300);
+
+    canvasContainer.empty();
     canvasContainer.append(renderedCanvas);
   }
 
